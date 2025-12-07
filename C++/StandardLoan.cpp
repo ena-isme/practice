5034586e5c9e4684861697104156454bc1a5ed80
ena-isme <dev.shinena@gmail.com>
2025-12-07 15:24:42 +0900

    주석 추가

--- C++/StandardLoan.cpp
+++ C++/StandardLoan.cpp
@@ -6,6 +6,7 @@ class StandardLoan : public LoanProduct {
     double annualRate_;
 public:
     //annualRate_는 계산할 때 공식에 넣음, 뒤에 _는 “멤버 변수”라는 표시
+    //r을 입력받아서 annualRate 에 넣어주는 의미
     explicit StandardLoan(double r) : annualRate_(r) {}
 
     string name() const override { return "Standard Loan"; }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # practice
+
+C++ 은 아래로 테스트
+| g++ -std=c++17 -O2 main.cpp LoanCalculator.cpp -o loan_calc
